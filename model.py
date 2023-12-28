2f45cd2add03d0c3e2502bb40b853df13bf6e010
Ravi-chandera <110690059+Ravi-chandera@users.noreply.github.com>
2023-12-29 00:13:10 +0530

    Update model.py

--- model.py
+++ model.py
@@ -26,12 +26,21 @@ def set_custom_prompt():
                             input_variables=['context', 'question'])
     return prompt
 
+#Retrieval QA Chain
+def retrieval_qa_chain(llm, prompt, db):
+    qa_chain = RetrievalQA.from_chain_type(llm=llm,
+                                       chain_type='stuff',
+                                       retriever=db.as_retriever(search_kwargs={'k': 2}),
+                                       return_source_documents=True,
+                                       chain_type_kwargs={'prompt': prompt}
+                                       )
+    return qa_chain
 
 #Loading the model
 def load_llm():
     # Load the locally downloaded model here
     llm = CTransformers(
-        model = "TheBloke/CodeLlama-34B-Instruct-GGUF",
+        model = "TheBloke/Llama-2-7B-Chat-GGML",
         model_type="llama",
         max_new_tokens = 512,
         temperature = 0.5
@@ -40,45 +49,50 @@ def load_llm():
 
 #QA Model Function
 def qa_bot():
-
+  
   embeddings = HuggingFaceEmbeddings(
-     model_name="sentence-transformers/all-MiniLM-L6-v2",
-     model_kwargs={'device': 'cuda'}
+      model_name="sentence-transformers/all-MiniLM-L6-v2", 
+      model_kwargs={'device': 'cuda'}
   )
   
+  db = FAISS.load_local(DB_FAISS_PATH, embeddings)  
   llm = load_llm()
   qa_prompt = set_custom_prompt()
   
-  return llm, qa_prompt
+  qa = retrieval_qa_chain(llm, qa_prompt, db)
+  return qa
 
 #output function
-def final_result(query, llm, prompt):
-  
-  response = llm(prompt.format(question=query))
-  return response
-    
+def final_result(query):
+    qa_result = qa_bot()
+    response = qa_result({'query': query})
+    return response
 
 #chainlit code
 @cl.on_chat_start
 async def start():
+    chain = qa_bot()
+    msg = cl.Message(content="Starting the bot...")
+    await msg.send()
+    msg.content = "Hi, Welcome to Medical Bot. What is your query?"
+    await msg.update()
 
-  llm, prompt = qa_bot()
-  
-  # Set llm and prompt in user session
-  bot_data = {"llm": llm, "prompt": prompt}
-  cl.user_session.set("bot", bot_data)
-
-  msg = cl.Message(content="Starting...")
-  await msg.send()
+    cl.user_session.set("chain", chain)
 
 @cl.on_message
 async def main(message: cl.Message):
+    chain = cl.user_session.get("chain") 
+    cb = cl.AsyncLangchainCallbackHandler(
+        stream_final_answer=True, answer_prefix_tokens=["FINAL", "ANSWER"]
+    )
+    cb.answer_reached = True
+    res = await chain.acall(message.content, callbacks=[cb])
+    answer = res["result"]
+    sources = res["source_documents"]
 
-  # Get llm and prompt from user session
-  bot_data = cl.user_session.get("bot")
-  llm = bot_data["llm"]
-  prompt = bot_data["prompt"]
-  
-  answer = final_result(message.content, llm, prompt)  
+    if sources:
+        answer += f"\nSources:" + str(sources)
+    else:
+        answer += "\nNo sources found"
 
-  await cl.Message(content=answer).send()
+    await cl.Message(content=answer).send()
