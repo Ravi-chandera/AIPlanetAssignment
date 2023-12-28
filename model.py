c05de574e56da2689cd12ad1e96927f692cd562b
Ravi-chandera <110690059+Ravi-chandera@users.noreply.github.com>
2023-12-28 23:52:30 +0530

    Update model.py

--- model.py
+++ model.py
@@ -26,15 +26,6 @@ def set_custom_prompt():
                             input_variables=['context', 'question'])
     return prompt
 
-#Retrieval QA Chain
-def retrieval_qa_chain(llm, prompt, db):
-    qa_chain = RetrievalQA.from_chain_type(llm=llm,
-                                       chain_type='stuff',
-                                       retriever=db.as_retriever(search_kwargs={'k': 2}),
-                                       return_source_documents=True,
-                                       chain_type_kwargs={'prompt': prompt}
-                                       )
-    return qa_chain
 
 #Loading the model
 def load_llm():
@@ -49,50 +40,37 @@ def load_llm():
 
 #QA Model Function
 def qa_bot():
-  
+
   embeddings = HuggingFaceEmbeddings(
-      model_name="sentence-transformers/all-MiniLM-L6-v2", 
-      model_kwargs={'device': 'cuda'}
+     model_name="sentence-transformers/all-MiniLM-L6-v2",
+     model_kwargs={'device': 'cuda'}
   )
   
-  db = FAISS.load_local(DB_FAISS_PATH, embeddings)  
   llm = load_llm()
   qa_prompt = set_custom_prompt()
   
-  qa = retrieval_qa_chain(llm, qa_prompt, db)
-  return qa
+  return llm, qa_prompt
 
 #output function
-def final_result(query):
-    qa_result = qa_bot()
-    response = qa_result({'query': query})
-    return response
+def final_result(query, llm, prompt):
+  
+  response = llm(prompt.format(question=query))
+  return response
+    
 
 #chainlit code
 @cl.on_chat_start
 async def start():
-    chain = qa_bot()
-    msg = cl.Message(content="Starting the bot...")
-    await msg.send()
-    msg.content = "Hi, Welcome to Medical Bot. What is your query?"
-    await msg.update()
-
-    cl.user_session.set("chain", chain)
+  llm, prompt = qa_bot()
+  
+  msg = cl.Message(content="Starting...")
+  await msg.send()  
 
 @cl.on_message
 async def main(message: cl.Message):
-    chain = cl.user_session.get("chain") 
-    cb = cl.AsyncLangchainCallbackHandler(
-        stream_final_answer=True, answer_prefix_tokens=["FINAL", "ANSWER"]
-    )
-    cb.answer_reached = True
-    res = await chain.acall(message.content, callbacks=[cb])
-    answer = res["result"]
-    sources = res["source_documents"]
 
-    if sources:
-        answer += f"\nSources:" + str(sources)
-    else:
-        answer += "\nNo sources found"
-
-    await cl.Message(content=answer).send()
+  llm, prompt = cl.user_session.get("bot")
+  
+  answer = final_result(message.content, llm, prompt)
+  
+  await cl.Message(content=answer).send()
